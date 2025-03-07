2e018f98102978ab0025aed09a18697609836b96
Andy Vo <danhvt0109@gmail.com>
2025-03-07 19:50:21 +0700

    fix: add error for callnback in native channel (#406)

--- src/channel/NativeChannel.js
+++ src/channel/NativeChannel.js
@@ -1,5 +1,7 @@
 import Channel, { encodeRequest, decodeUnaryResponse } from "./Channel.js";
 import * as base64 from "../encoding/base64.js";
+import HttpError from "../http/HttpError.js";
+import HttpStatus from "../http/HttpStatus.js";
 
 export default class NativeChannel extends Channel {
     /**
@@ -31,68 +33,79 @@ export default class NativeChannel extends Channel {
      */
     _createUnaryClient(serviceName) {
         return async (method, requestData, callback) => {
-            const data = base64.encode(
-                new Uint8Array(encodeRequest(requestData))
-            );
-
-            const response = await fetch(
-                `${this._address}/proto.${serviceName}/${method.name}`,
-                {
-                    method: "POST",
-                    headers: {
-                        "content-type": "application/grpc-web-text",
-                        "x-user-agent": "hedera-sdk-js/v2",
-                        "x-accept-content-transfer-encoding": "base64",
-                        "x-grpc-web": "1",
-                    },
-                    body: data,
+            try {
+                const data = base64.encode(
+                    new Uint8Array(encodeRequest(requestData))
+                );
+    
+                const response = await fetch(
+                    `${this._address}/proto.${serviceName}/${method.name}`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/grpc-web-text",
+                            "x-user-agent": "hedera-sdk-js/v2",
+                            "x-accept-content-transfer-encoding": "base64",
+                            "x-grpc-web": "1",
+                        },
+                        body: data,
+                    }
+                );
+    
+                if (!response.ok) {
+                    const error = new HttpError(
+                        HttpStatus._fromValue(response.status),
+                    );
+                    callback(error, null);
                 }
-            );
-
-            const blob = await response.blob();
-
-            /** @type {string} */
-            const responseData = await new Promise((resolve, reject) => {
-                const reader = new FileReader();
-                reader.readAsDataURL(blob);
-                reader.onloadend = () => {
-                    resolve(/** @type {string} */ (reader.result));
-                };
-                reader.onerror = reject;
-            });
-
-            let responseBuffer;
-            if (
-                responseData.startsWith("data:application/octet-stream;base64,")
-            ) {
-                responseBuffer = base64.decode(
-                    responseData.split(
-                        "data:application/octet-stream;base64,"
-                    )[1]
-                );
-            } else if (
-                responseData.startsWith(
-                    "data:application/grpc-web+proto;base64,"
-                )
-            ) {
-                responseBuffer = base64.decode(
-                    responseData.split(
+    
+                const blob = await response.blob();
+    
+                /** @type {string} */
+                const responseData = await new Promise((resolve, reject) => {
+                    const reader = new FileReader();
+                    reader.readAsDataURL(blob);
+                    reader.onloadend = () => {
+                        resolve(/** @type {string} */ (reader.result));
+                    };
+                    reader.onerror = reject;
+                });
+    
+                let responseBuffer;
+                if (
+                    responseData.startsWith("data:application/octet-stream;base64,")
+                ) {
+                    responseBuffer = base64.decode(
+                        responseData.split(
+                            "data:application/octet-stream;base64,"
+                        )[1]
+                    );
+                } else if (
+                    responseData.startsWith(
                         "data:application/grpc-web+proto;base64,"
-                    )[1]
+                    )
+                ) {
+                    responseBuffer = base64.decode(
+                        responseData.split(
+                            "data:application/grpc-web+proto;base64,"
+                        )[1]
+                    );
+                } else {
+                    throw new Error(
+                        `Expected response data to be base64 encode with a 'data:application/octet-stream;base64,' or 'data:application/grpc-web+proto;base64,' prefix, but found: ${responseData}`
+                    );
+                }
+    
+                const unaryResponse = decodeUnaryResponse(
+                    responseBuffer.buffer,
+                    responseBuffer.byteOffset,
+                    responseBuffer.byteLength
                 );
-            } else {
-                throw new Error(
-                    `Expected response data to be base64 encode with a 'data:application/octet-stream;base64,' or 'data:application/grpc-web+proto;base64,' prefix, but found: ${responseData}`
-                );
+    
+                callback(null, unaryResponse);
+            } catch (error) {
+                callback(error, null);
             }
-
-            const unaryResponse = decodeUnaryResponse(
-                responseBuffer.buffer,
-                responseBuffer.byteOffset,
-                responseBuffer.byteLength
-            );
-
-            callback(null, unaryResponse);
         };
     }
 }
