007c2157557b5825832bb627bdafd7658ddc0e96
Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
2025-05-02 17:10:01 +0300

    fix: backwards compatible wallets
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

--- test/integration/LocalWalletTest.js
+++ test/integration/LocalWalletTest.js
@@ -1,17 +1,12 @@
-import {
-    Wallet,
-    LocalProvider,
-    PrivateKey,
-    AccountId,
-} from "../../src/index.js";
+import { Wallet, LocalProvider } from "../../src/index.js";
 
 describe("LocalWallet", function () {
     it("can fetch wallet's info", async function () {
-        const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
-        const operatorKey = PrivateKey.fromStringED25519(
+        const wallet = new Wallet(
+            process.env.OPERATOR_ID,
             process.env.OPERATOR_KEY,
+            new LocalProvider(),
         );
-        const wallet = new Wallet(operatorId, operatorKey, new LocalProvider());
 
         const info = await wallet.getAccountInfo();
 
