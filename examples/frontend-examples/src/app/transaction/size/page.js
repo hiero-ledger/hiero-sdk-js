7a515adfd33f20e5ea82ce5e61825f38183245de
Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
2025-06-17 14:11:19 +0300

    feat: update transaction size example
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

--- examples/frontend-examples/src/app/transaction/size/page.js
+++ examples/frontend-examples/src/app/transaction/size/page.js
@@ -12,30 +12,21 @@ import {
 } from "@hashgraph/sdk";
 
 const Home = () => {
-    const [accountId, setAccountId] = useState("");
     const [initialBalance, setInitialBalance] = useState("");
     const [key, setKey] = useState("");
     const [transactionSize, setTransactionSize] = useState(0);
 
-    const PRIVATE_KEY =
+    const DUMMY_ACCOUNT_ID = "0.0.10022";
+    const DUMMY_PRIVATE_KEY =
         "0xa608e2130a0a3cb34f86e757303c862bee353d9ab77ba4387ec084f881d420d4";
-    const OPERATOR_ID = "0.0.10022";
     const client = WebClient.forTestnet().setOperator(
-        OPERATOR_ID,
-        PrivateKey.fromStringED25519(PRIVATE_KEY),
+        DUMMY_ACCOUNT_ID,
+        PrivateKey.fromStringED25519(DUMMY_PRIVATE_KEY),
     );
 
     return (
         <div className="flex items-center justify-center">
             <form className="flex flex-col gap-4">
-                <TextField
-                    label="Account ID"
-                    variant="outlined"
-                    className="block w-full"
-                    onChange={(e) => {
-                        setAccountId(e.target.value);
-                    }}
-                />
                 <TextField
                     label="Initial Balance"
                     variant="outlined"
@@ -65,9 +56,6 @@ const Home = () => {
                         }
                         if (initialBalance) {
                             tx.setInitialBalance(Hbar.from(initialBalance));
-                        }
-                        if (accountId) {
-                            tx.setAccountId(accountId);
                         }
                         const txSize = tx.freezeWith(client).size;
                         setTransactionSize(txSize);
