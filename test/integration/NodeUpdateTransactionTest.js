3356c6f266fc199c46d2efbde0d59345e47d9586
Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
2025-08-05 00:23:48 +0300

    test: clear web proxy endpoint from node
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

---
+++ test/integration/NodeUpdateTransactionTest.js
@@ -0,0 +1,96 @@
+import { setTimeout } from "timers/promises";
+import {
+    AccountId,
+    NodeUpdateTransaction,
+    PrivateKey,
+    ServiceEndpoint,
+} from "../../src/exports.js";
+import IntegrationTestEnv from "./client/NodeIntegrationTestEnv.js";
+
+// eslint-disable-next-line vitest/no-disabled-tests
+describe("NodeUpdateTransaction", function () {
+    let env;
+
+    beforeAll(async function () {
+        env = await IntegrationTestEnv.new();
+    });
+
+    it("should create and update a network node", async function () {
+        // Constants for better readability
+        const OPERATOR_ACCOUNT_ID = "0.0.2";
+        const OPERATOR_PRIVATE_KEY =
+            "302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137";
+        const NODE_ACCOUNT_ID = "0.0.3";
+        const MIRROR_NODE_API_BASE_URL =
+            "http://localhost:5551/api/v1/network/nodes";
+        const API_RESPONSE_DELAY_MS = 2500;
+        const TEST_DOMAIN_NAME = "test.com";
+        const TEST_PORT = 123456;
+        const TARGET_NODE_ID = 0;
+
+        // Set the operator to be account 0.0.2
+        const operatorPrivateKey =
+            PrivateKey.fromStringED25519(OPERATOR_PRIVATE_KEY);
+        const operatorAccount = AccountId.fromString(OPERATOR_ACCOUNT_ID);
+
+        env.client.setOperator(operatorAccount, operatorPrivateKey);
+
+        // The account of the new node
+        const nodeAccount = AccountId.fromString(NODE_ACCOUNT_ID);
+
+        // Generate admin key
+        const nodeAdminKey = PrivateKey.generateED25519();
+        // Create the node
+
+        // Update the node
+        const updatedGrpcEndpoint = new ServiceEndpoint()
+            .setDomainName(TEST_DOMAIN_NAME)
+            .setPort(TEST_PORT);
+
+        await (
+            await (
+                await new NodeUpdateTransaction()
+                    .setNodeId(new AccountId(3))
+                    .setAccountId(nodeAccount)
+                    .setDeclineReward(false)
+                    .setGrpcWebProxyEndpoint(updatedGrpcEndpoint)
+                    .freezeWith(env.client)
+                    .sign(nodeAdminKey)
+            ).execute(env.client)
+        ).getReceipt(env.client);
+
+        await setTimeout(API_RESPONSE_DELAY_MS);
+        const firstResponse = await fetch(
+            `${MIRROR_NODE_API_BASE_URL}?node.id=${TARGET_NODE_ID}`,
+        );
+        const firstNodeData = await firstResponse.json();
+        expect(firstNodeData.nodes[0].grpc_proxy_endpoint.domainName).toEqual(
+            updatedGrpcEndpoint.domainName,
+        );
+
+        await (
+            await (
+                await new NodeUpdateTransaction()
+                    .setNodeId(new AccountId(3))
+                    .setAccountId(nodeAccount)
+                    .setDeclineReward(false)
+                    .clearGrpcWebProxyEndpoint()
+                    .freezeWith(env.client)
+                    .sign(nodeAdminKey)
+            ).execute(env.client)
+        ).getReceipt(env.client);
+
+        await setTimeout(API_RESPONSE_DELAY_MS);
+        const secondResponse = await fetch(
+            `${MIRROR_NODE_API_BASE_URL}?node.id=${TARGET_NODE_ID}`,
+        );
+        const secondNodeData = await secondResponse.json();
+        expect(secondNodeData.nodes[0].grpc_proxy_endpoint.domainName).toEqual(
+            undefined,
+        );
+    });
+
+    afterAll(async function () {
+        await env.close();
+    });
+});
