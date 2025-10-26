fdbc425aca7b75194bedb4850bdc245ae315b1d9
Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
2025-10-26 23:37:21 +0200

    feat: add exports for hooks
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

--- src/exports.js
+++ src/exports.js
@@ -72,6 +72,23 @@ export { default as FeeDataType } from "./FeeDataType.js";
 export { default as FeeSchedule } from "./FeeSchedule.js";
 export { default as FeeSchedules } from "./FeeSchedules.js";
 export { default as FileAppendTransaction } from "./file/FileAppendTransaction.js";
+export { default as HookCreationDetails } from "./hooks/HookCreationDetails.js";
+export { default as LambdaEvmHook } from "./hooks/LambdaEvmHook.js";
+export { HookExtensionPoint } from "./hooks/HookExtensionPoint.js";
+export { default as FungibleHookCall } from "./hooks/FungibleHookCall.js";
+export { default as NftHookCall } from "./hooks/NftHookCall.js";
+export { default as EvmHookCall } from "./hooks/EvmHookCall.js";
+export { default as FungibleHookType } from "./hooks/FungibleHookType.js";
+export { default as NftHookType } from "./hooks/NftHookType.js";
+export { default as HookId } from "./hooks/HookId.js";
+export { default as HookEntityId } from "./hooks/HookEntityId.js";
+export { default as LambdaSStoreTransaction } from "./hooks/LambdaSStoreTransaction.js";
+export {
+    LambdaStorageUpdate,
+    LambdaStorageSlot,
+    LambdaMappingEntries,
+} from "./hooks/LambdaStorageUpdate.js";
+export { default as LambdaMappingEntry } from "./hooks/LambdaMappingEntry.js";
 export { default as FileContentsQuery } from "./file/FileContentsQuery.js";
 export { default as FileCreateTransaction } from "./file/FileCreateTransaction.js";
 export { default as FileDeleteTransaction } from "./file/FileDeleteTransaction.js";
