130d68b982cc830088b244434ff2a9e90b6f99d3
Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
2025-10-27 16:03:37 +0200

    fix: use ecdsa instead of ed25519
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

--- examples/transfer-transaction-hooks-example.js
+++ examples/transfer-transaction-hooks-example.js
@@ -39,7 +39,7 @@ async function main() {
     }
 
     const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
-    const operatorKey = PrivateKey.fromStringED25519(process.env.OPERATOR_KEY);
+    const operatorKey = PrivateKey.fromStringECDSA(process.env.OPERATOR_KEY);
     const network = process.env.HEDERA_NETWORK;
     const client = Client.forName(network).setOperator(operatorId, operatorKey);
 
