8ccc55b80e4a3562ece40590ff5c7d1f169063f7
Roger Barker <roger.barker@swirldslabs.com>
2025-11-12 13:14:17 -0600

    chore: Update lock-files galore!!!
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>

--- examples/get-account-info.js
+++ examples/get-account-info.js
@@ -1,4 +1,9 @@
-import { Wallet, LocalProvider, AccountInfoQuery, Hbar } from "@hiero-ledger/sdk";
+import {
+    Wallet,
+    LocalProvider,
+    AccountInfoQuery,
+    Hbar,
+} from "@hiero-ledger/sdk";
 
 import dotenv from "dotenv";
 
