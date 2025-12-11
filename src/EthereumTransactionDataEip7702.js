0b9905b4e9909d11c2548d0b3626f499694d4ab4
Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
2025-12-11 13:49:17 +0200

    fix: lint issues
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

--- src/EthereumTransactionDataEip7702.js
+++ src/EthereumTransactionDataEip7702.js
@@ -66,23 +66,24 @@ export default class EthereumTransactionDataEip7702 extends EthereumTransactionD
         }
 
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const decoded = /** @type {any[]} */ (rlp.decode(bytes.subarray(1)));
+        const decoded = /** @type {string[]} */ (rlp.decode(bytes.subarray(1)));
 
         if (!Array.isArray(decoded)) {
             throw new Error("ethereum data is not a list");
         }
 
-        if (decoded.length !== 12) {
-            // Type 4 per spec: [chain_id, nonce, max_priority_fee_per_gas, max_fee_per_gas, gas_limit, destination, data, access_list, [[contract_code, y_parity, r, s], ...], signature_y_parity, signature_r, signature_s]
+        if (decoded.length !== 13) {
             throw new Error("invalid ethereum transaction data");
         }
 
         // Decode authorization list: array of [chainId, contractAddress, nonce, yParity, r, s] tuples
         // Authorization list can be empty (empty array is valid)
-        if (!Array.isArray(decoded[8])) {
+        if (!Array.isArray(decoded[9])) {
             throw new Error("authorization list must be an array");
         }
-        const authorizationList = /** @type {string[]} */ (decoded[8]).map(
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        const authorizationList = /** @type {string[]} */ (decoded[9]).map(
             (authTuple) => {
                 if (!Array.isArray(authTuple) || authTuple.length !== 6) {
                     throw new Error(
@@ -90,34 +91,40 @@ export default class EthereumTransactionDataEip7702 extends EthereumTransactionD
                     );
                 }
                 return [
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
                     hex.decode(/** @type {string} */ (authTuple[0])), // chainId
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
                     hex.decode(/** @type {string} */ (authTuple[1])), // contractAddress (20 bytes)
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
                     hex.decode(/** @type {string} */ (authTuple[2])), // nonce
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
                     hex.decode(/** @type {string} */ (authTuple[3])), // yParity (0 or 1)
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
                     hex.decode(/** @type {string} */ (authTuple[4])), // r (32 bytes)
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
                     hex.decode(/** @type {string} */ (authTuple[5])), // s (32 bytes)
                 ];
             },
         );
 
         return new EthereumTransactionDataEip7702({
-            chainId: hex.decode(/** @type {string} */ (decoded[0])), // chain_id
-            nonce: hex.decode(/** @type {string} */ (decoded[1])), // nonce
-            maxPriorityGas: hex.decode(/** @type {string} */ (decoded[2])), // max_priority_fee_per_gas
-            maxGas: hex.decode(/** @type {string} */ (decoded[3])), // max_fee_per_gas
-            gasLimit: hex.decode(/** @type {string} */ (decoded[4])), // gas_limit
-            to: hex.decode(/** @type {string} */ (decoded[5])), // destination
-            value: new Uint8Array(), // value is not in EIP-7702 spec
-            callData: hex.decode(/** @type {string} */ (decoded[6])), // data
+            chainId: hex.decode(/** @type {string} */ (decoded[0])),
+            nonce: hex.decode(/** @type {string} */ (decoded[1])),
+            maxPriorityGas: hex.decode(/** @type {string} */ (decoded[2])),
+            maxGas: hex.decode(/** @type {string} */ (decoded[3])),
+            gasLimit: hex.decode(/** @type {string} */ (decoded[4])),
+            to: hex.decode(/** @type {string} */ (decoded[5])),
+            value: hex.decode(/** @type {string} */ (decoded[6])),
+            callData: hex.decode(/** @type {string} */ (decoded[7])),
             // @ts-ignore
-            accessList: /** @type {string[]} */ (decoded[7]).map((v) =>
+            accessList: /** @type {string[]} */ (decoded[8]).map((v) =>
                 hex.decode(v),
-            ), // access_list
+            ),
             // @ts-ignore
-            authorizationList: authorizationList, // [[contract_code, y_parity, r, s], ...]
-            recId: hex.decode(/** @type {string} */ (decoded[9])), // signature_y_parity
-            r: hex.decode(/** @type {string} */ (decoded[10])), // signature_r
-            s: hex.decode(/** @type {string} */ (decoded[11])), // signature_s
+            authorizationList: authorizationList,
+            recId: hex.decode(/** @type {string} */ (decoded[10])),
+            r: hex.decode(/** @type {string} */ (decoded[11])),
+            s: hex.decode(/** @type {string} */ (decoded[12])),
         });
     }
 
@@ -125,35 +132,22 @@ export default class EthereumTransactionDataEip7702 extends EthereumTransactionD
      * @returns {Uint8Array}
      */
     toBytes() {
-        // Encode authorization list: each entry is [chainId, contractAddress, nonce, yParity, r, s]
-        const encodedAuthorizationList = this.authorizationList.map(
-            ([chainId, contractAddress, nonce, yParity, r, s]) => [
-                hex.encode(chainId),
-                hex.encode(contractAddress),
-                hex.encode(nonce),
-                hex.encode(yParity),
-                hex.encode(r),
-                hex.encode(s),
-            ],
-        );
-
-        // RLP encoding order per spec: [chain_id, nonce, max_priority_fee_per_gas, max_fee_per_gas, gas_limit, destination, data, access_list, [[contract_code, y_parity, r, s], ...], signature_y_parity, signature_r, signature_s]
         const encoded = rlp.encode([
-            this.chainId, // chain_id
-            this.nonce, // nonce
-            this.maxPriorityGas, // max_priority_fee_per_gas
-            this.maxGas, // max_fee_per_gas
-            this.gasLimit, // gas_limit
-            this.to, // destination
+            this.chainId,
+            this.nonce,
+            this.maxPriorityGas,
+            this.maxGas,
+            this.gasLimit,
+            this.to,
             this.value,
-            this.callData, // data
-            this.accessList, // access_list
-            encodedAuthorizationList, // [[contract_code, y_parity, r, s], ...]
-            this.recId, // signature_y_parity
-            this.r, // signature_r
-            this.s, // signature_s
+            this.callData,
+            this.accessList,
+            this.authorizationList,
+            this.recId,
+            this.r,
+            this.s,
         ]);
-        return hex.decode("04" + encoded.substring(2)); // Type 4 prefix
+        return hex.decode("04" + encoded.substring(2));
     }
 
     /**
