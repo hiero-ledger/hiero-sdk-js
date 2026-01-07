db2ef08eb9374915fd3323717ef21e79167e1771
Mario <mario.dimitrov@limechain.tech>
2026-01-07 15:07:30 +0200

    feat(TCK): implement TokenInfoQuery endpoint (#3593)
    Signed-off-by: Mario Dimitrov <mario.dimitrov@limechain.tech>

--- tck/response/token.ts
+++ tck/response/token.ts
@@ -15,3 +15,35 @@ export interface TokenBurnResponse {
     readonly newTotalSupply?: string;
     readonly status: string;
 }
+
+export interface TokenInfoQueryResponse {
+    readonly tokenId?: string;
+    readonly name?: string;
+    readonly symbol?: string;
+    readonly decimals?: number;
+    readonly totalSupply?: string;
+    readonly treasuryAccountId?: string;
+    readonly adminKey?: string;
+    readonly kycKey?: string;
+    readonly freezeKey?: string;
+    readonly pauseKey?: string;
+    readonly wipeKey?: string;
+    readonly supplyKey?: string;
+    readonly feeScheduleKey?: string;
+    readonly metadataKey?: string;
+    readonly defaultFreezeStatus?: boolean | null;
+    readonly defaultKycStatus?: boolean | null;
+    readonly pauseStatus?: string;
+    readonly isDeleted?: boolean;
+    readonly autoRenewAccountId?: string;
+    readonly autoRenewPeriod?: string;
+    readonly expirationTime?: string;
+    readonly tokenMemo?: string;
+    readonly customFees?: any[];
+    readonly tokenType?: string;
+    readonly supplyType?: string;
+    readonly maxSupply?: string;
+    readonly metadata?: string;
+    readonly ledgerId?: string;
+    readonly cost?: string;
+}
