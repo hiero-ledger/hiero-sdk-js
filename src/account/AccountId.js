7ce3215827c2e8dc6da8a2db5830f50917046ec9
AZ & QZ <dev@local>
2026-03-02 10:19:00 -0700

    refactor: use generic Client<Channel, MirrorChannel> in entity ID classes
    - Fix wildcard type annotations in AccountId, ContractId, FileId, ScheduleId, TokenId
    - Replaces Client<*, *> with properly typed Client<Channel, MirrorChannel>
    - Resolves eslint jsdoc/reject-any-type warnings
    - Related to #3798
    
    Signed-off-by: AZ & QZ <dev@local>

--- src/account/AccountId.js
+++ src/account/AccountId.js
@@ -11,7 +11,9 @@ import * as hex from ".././encoding/hex.js";
 import { isLongZeroAddress } from "../util.js";
 
 /**
- * @typedef {import("../client/Client.js").default<*, *>} Client
+ * @typedef {import("../channel/Channel.js").default} Channel
+ * @typedef {import("../channel/MirrorChannel.js").default} MirrorChannel
+ * @typedef {import("../client/Client.js").default<Channel, MirrorChannel>} Client
  */
 
 /**
