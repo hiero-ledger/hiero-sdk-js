a9e242d81128bae68719edf7df9329146023db22
Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
2026-03-18 11:31:09 +0200

    chore: disable eslint error
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

--- src/cryptography/sha384.browser.js
+++ src/cryptography/sha384.browser.js
@@ -8,8 +8,8 @@ export async function digest(data) {
     // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
     // This will be executed in a browser environment so the crypto object should be available if its
     // in secure context.
-    // eslint-disable-next-line n/no-unsupported-features/node-builtins
     const input =
-        data.buffer instanceof ArrayBuffer ? data : new Uint8Array(data); // copy SharedArrayBuffer-backed data into plain ArrayBuffer
+        data.buffer instanceof ArrayBuffer ? data : new Uint8Array(data);
+    // eslint-disable-next-line n/no-unsupported-features/node-builtins
     return new Uint8Array(await window.crypto.subtle.digest("SHA-384", input));
 }
