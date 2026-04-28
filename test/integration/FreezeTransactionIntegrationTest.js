f106ea2b5bfc12a05209ab4b85a28b3113305a75
venilinvasilev <venilin.vasilev@gmail.com>
2026-04-28 21:15:37 +0300

    test: remove skip
    Signed-off-by: venilinvasilev <venilin.vasilev@gmail.com>

--- test/integration/FreezeTransactionIntegrationTest.js
+++ test/integration/FreezeTransactionIntegrationTest.js
@@ -8,7 +8,7 @@ import {
 } from "../../src/exports.js";
 import IntegrationTestEnv from "./client/NodeIntegrationTestEnv.js";
 
-describe.skip("FreezeTransaction", function () {
+describe("FreezeTransaction", function () {
     let client;
 
     beforeAll(async function () {
