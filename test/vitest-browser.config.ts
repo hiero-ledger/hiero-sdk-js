2464b75a6038be52249ff5fe2c35c17441ea6e6c
G.M Erandi Hansika <erandi2287hansika@gmail.com>
2026-05-29 00:36:59 +0530

    chore: exclude static address book re-exports from vitest coverage (#4041)
    Signed-off-by: G.M Erandi Hansika <erandi2287hansika@gmail.com>
    Signed-off-by: Erandi Hansika <erandi2287hansika@gmail.com>
    Signed-off-by: G. M Erandi Hansika <erandi2287hansika@gmail.com>

--- test/vitest-browser.config.ts
+++ test/vitest-browser.config.ts
@@ -23,7 +23,12 @@ export default defineConfig({
         testTimeout: 120000,
         retry: 1,
         coverage: {
-            include: ["src/**/*.js"],
+           include: ["src/**/*.js"],
+            exclude: [
+                "src/client/addressbooks/mainnet.js",
+                "src/client/addressbooks/previewnet.js",
+                "src/client/addressbooks/testnet.js",
+            ],
             provider: "v8",
             reporter: ["text-summary", "lcov"],
             reportsDirectory: "./coverage/browser",
