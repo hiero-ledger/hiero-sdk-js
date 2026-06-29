0516de89eb0d621518ab779464e3c04fb34bab1e
Fantomasa <ivailoinfo@gmail.com>
2026-06-29 10:34:22 +0300

    feat: make Ethereum transaction data envelope constructors public
    Signed-off-by: Fantomasa <ivailoinfo@gmail.com>

--- src/EthereumTransactionDataEip7702.js
+++ src/EthereumTransactionDataEip7702.js
@@ -36,7 +36,6 @@ import CACHE from "./Cache.js";
 
 export default class EthereumTransactionDataEip7702 extends EthereumTransactionData {
     /**
-     * @private
      * @param {object} props
      * @param {Uint8Array} props.chainId
      * @param {Uint8Array} props.nonce
