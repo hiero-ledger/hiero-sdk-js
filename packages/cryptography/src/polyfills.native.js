4be8042462b2fc971b0ba7b114621a19bde30917
Dian Deskov <dian.deskov@limechain.tech>
2026-07-14 11:14:32 +0300

    fix: remove react-native-get-random-values from cryptography package (#4233)
    Signed-off-by: Dian Deskov <dian.deskov@limechain.tech>

--- packages/cryptography/src/polyfills.native.js
+++ packages/cryptography/src/polyfills.native.js
@@ -1,2 +1,4 @@
-import "react-native-get-random-values";
+// NOTE: React Native / Expo apps must polyfill `crypto.getRandomValues`
+// themselves by importing `react-native-get-random-values` at the app root
+// before importing the SDK.
 global.process.nextTick = setImmediate;
